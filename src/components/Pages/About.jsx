4f8087d9e51db2de3d6d32708fc9fc079392c889
My nikname "ton <81211115+Kiattisak9905@users.noreply.github.com>
2022-04-03 12:35:31 +0700

    create

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <p className='J'>ขวด  5 บาท-
+          
+        `</p>
+    </div>
+  )
+}
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <p className='P2'>น้ำทิพย์</p>
+    </div>
+
+  )
+}
