51c362d03d39f1371907edd5889e36c7ff80a235
Kiattisak9905 <81211115+Kiattisak9905@users.noreply.github.com>
2022-04-04 18:41:41 +0700

    Revert "vscode format setting"
    This reverts commit f24d0330535a7f00907e94e5b7c24e13955b4726.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './components/Style/index'
-import './App.css'
+import './components/Style/index';
+import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useNavigate, Link} from 'react-router-dom'
 
-import User from './components/Pages/User'
-import Dashboard from './components/Pages/Dashboard'
-import Report from './components/Pages/Report'
-import * as React from 'react'
+import User from './components/Pages/User';
+import Dashboard from './components/Pages/Dashboard';
+import Report from './components/Pages/Report';
+import * as React from 'react';
 
-import { styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import Navbar from './components/Layout/Navbar'
 import Sidebar from './components/Layout/Sidebar'
@@ -22,27 +22,27 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-}))
+}));
 function App() {
-  const [open, setOpen] = React.useState(false)
 
-  return (
-    <BrowserRouter>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
+  const [open, setOpen] = React.useState(false);
+
+        return <BrowserRouter>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
         <Navbar open={open} setOpen={setOpen} />
-        <Sidebar open={open} setOpen={setOpen} />
+        <Sidebar open={open} setOpen={setOpen}/>
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <DrawerHeader />
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/report" element={<Report />} />
-          </Routes>
+        <DrawerHeader />
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/user' element={<User />} />
+          <Route path='/report' element={<Report />} />
+        </Routes>
         </Box>
-      </Box>
-    </BrowserRouter>
-  )
+        </Box>
+        </BrowserRouter>
 }
 
-export default App
+
+export default App;
